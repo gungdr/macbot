80e312327190fede2d9cde2ae38881de080dd774
agung <agungdwiriansah@gmail.com>
2019-10-16 21:33:06 +0700

    feat(install) : remove unneeded script

--- install.sh
+++ install.sh
@@ -67,28 +67,6 @@ run sudo cp ./files/.inputrc ~/.inputrc
 echo "Disable sudden motion sensor. (Not useful for SSDs)."
 run sudo pmset -a sms 0
 
-echo "Use 24-hour time. Use the format EEE MMM d  H:mm:ss"
-run defaults write com.apple.menuextra.clock DateFormat -string 'EEE MMM d  H:mm:ss'
-
-echo "Disable press-and-hold for keys in favor of key repeat."
-run defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-
-echo "Set a fast keyboard repeat rate, after a good initial delay."
-run defaults write NSGlobalDomain KeyRepeat -int 1
-run defaults write NSGlobalDomain InitialKeyRepeat -int 25
-
-echo "Disable auto-correct."
-run defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-
-echo "Speed up mission control animations."
-run defaults write com.apple.dock expose-animation-duration -float 0.1
-
-echo "Remove the auto-hiding dock delay."
-run defaults write com.apple.dock autohide-delay -int 0
-
-echo "Use the dark theme."
-run defaults write ~/Library/Preferences/.GlobalPreferences AppleInterfaceStyle -string "Dark"
-
 echo "Save screenshots in PNG format."
 run defaults write com.apple.screencapture type -string png
 
@@ -96,26 +74,8 @@ echo "Save screenshots to user screenshots directory instead of desktop."
 run mkdir ~/screenshots
 run defaults write com.apple.screencapture location -string ~/screenshots
 
-echo "Disable menu transparency."
-run defaults write com.apple.universalaccess reduceTransparency -int 1
-
-echo "Turn off increased contrast. macOS 10.14 causes ugly white borders."
-run defaults write com.apple.universalaccess increaseContrast -int 0
-
-echo "Disable mouse enlargement with jiggle."
-run defaults write ~/Library/Preferences/.GlobalPreferences CGDisableCursorLocationMagnification -bool true
-
-echo "Disable annoying UI error sounds."
-run defaults write com.apple.systemsound com.apple.sound.beep.volume -int 0
-run defaults write com.apple.sound.beep feedback -int 0
-run defaults write com.apple.systemsound com.apple.sound.uiaudio.enabled -int 0
-run osascript -e 'set volume alert volume 0'
-
 echo "Show all filename extensions."
 run defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-echo "Disable the warning when changing a file extension."
-run defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
 echo "Use list view in all Finder windows by default."
 run defaults write com.apple.finder FXPreferredViewStyle -string '"Nlsv"'
@@ -135,127 +95,11 @@ run defaults write _FXShowPosixPathInTitle com.apple.finder -int 1
 echo "Don't write DS_Store files to network shares."
 run defaults write DSDontWriteNetworkStores com.apple.desktopservices -int 1
 
-echo "Don't ask to use external drives as a Time Machine backup."
-run defaults write DoNotOfferNewDisksForBackup com.apple.TimeMachine -int 1
-
-echo "Disable natural scrolling."
-run defaults write ~/Library/Preferences/.GlobalPreferences com.apple.swipescrolldirection -bool false
-
-# Security And Privacy Improvements
-echo "Disable Safari from auto-filling sensitive data."
-run defaults write ~/Library/Preferences/com.apple.Safari AutoFillCreditCardData -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari AutoFillFromAddressBook -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari AutoFillMiscellaneousForms -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari AutoFillPasswords -bool false
-
-echo "Disable Safari from automatically opening files."
-run defaults write ~/Library/Preferences/com.apple.Safari AutoOpenSafeDownloads -bool false
-
-echo "Always block cookies and local storage in Safari."
-run defaults write ~/Library/Preferences/com.apple.Safari BlockStoragePolicy -bool false
-
-echo "Enable Safari warnings when visiting fradulent websites."
-run defaults write ~/Library/Preferences/com.apple.Safari WarnAboutFraudulentWebsites -bool true
-
-echo "Disable javascript in Safari."
-run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptEnabled -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari WebKitJavaScriptEnabled -bool false
-
-echo "Block popups in Safari."
-run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari WebKitJavaScriptCanOpenWindowsAutomatically -bool false
-
-echo "Disable plugins and extensions in Safari."
-run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2WebGLEnabled -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2PluginsEnabled -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari WebKitPluginsEnabled -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari ExtensionsEnabled -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari PlugInFirstVisitPolicy PlugInPolicyBlock
-run defaults write ~/Library/Preferences/com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled -bool false
-run defaults write ~/Library/Preferences/com.apple.Safari WebKitJavaEnabled -bool false
-
-echo "Safari should treat SHA-1 certificates as insecure."
-run defaults write ~/Library/Preferences/com.apple.Safari TreatSHA1CertificatesAsInsecure -bool true
-
-echo "Disable pre-loading websites with high search rankings."
-run defaults write ~/Library/Preferences/com.apple.Safari PreloadTopHit -bool false
-
-echo "Disable Safari search engine suggestions."
-run defaults write ~/Library/Preferences/com.apple.Safari SuppressSearchSuggestions -bool true
-
-echo "Enable Do-Not-Track HTTP header in Safari."
-run defaults write ~/Library/Preferences/com.apple.Safari SendDoNotTrackHTTPHeader -bool true
-
-echo "Disable pdf viewing in Safari."
-run defaults write ~/Library/Preferences/com.apple.Safari WebKitOmitPDFSupport -bool true
-
-echo "Display full website addresses in Safari."
-run defaults write ~/Library/Preferences/com.apple.Safari ShowFullURLInSmartSearchField -bool true
-
-echo "Disable loading remote content in emails in Apple Mail."
-run defaults write ~/Library/Preferences/com.apple.mail-shared DisableURLLoading -bool true
-
-echo "Send junk mail to the junk mail box in Apple Mail."
-run defaults write ~/Library/Containers/com.apple.mail/Data/Library/Preferences/com.apple.mail JunkMailBehavior -int 2
-
 echo "Disable spotlight universal search (don't send info to Apple)."
 run defaults write com.apple.safari UniversalSearchEnabled -int 0
 
 echo "Disable Spotlight Suggestions, Bing Web Search, and other leaky data."
 run python ./fix_leaky_data.py
-
-echo "Disable Captive Portal Hijacking Attack."
-run defaults write /Library/Preferences/SystemConfiguration/com.apple.captive.control Active -bool false
-
-echo "Set screen to lock as soon as the screensaver starts."
-run defaults write com.apple.screensaver askForPassword -int 1
-run defaults write com.apple.screensaver askForPasswordDelay -int 0
-
-echo "Don't default to saving documents to iCloud."
-run defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
-
-echo "Disable crash reporter."
-run defaults write com.apple.CrashReporter DialogType none
-
-echo "Enable Stealth Mode. Computer will not respond to ICMP ping requests or connection attempts from a closed TCP/UDP port."
-run defaults write /Library/Preferences/com.apple.alf stealthenabled -bool true
-
-echo "Set all network interfaces to use Cloudflare DNS (1.1.1.1)."
-run bash ./use_cloudflare_dns.sh
-
-echo "Disable wake on network access."
-run systemsetup -setwakeonnetworkaccess off
-
-echo "Disable Bonjour multicast advertisements."
-run defaults write /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdvertisements -bool YES
-
-# This is disabled by default, but sometimes people turn it on and forget to turn it back off again.
-echo "Turn off remote desktop access."
-run sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -configure -access -off
-
-echo "Enable Mac App Store automatic updates."
-run defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
-
-echo "Check for Mac App Store updates daily."
-run defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
-
-echo "Download Mac App Store updates in the background."
-run defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
-
-echo "Install Mac App Store system data files & security updates."
-run defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
-
-echo "Turn on Mac App Store auto-update."
-run defaults write com.apple.commerce AutoUpdate -bool true
-
-# Blocklists
-
-echo "Block all Facebook domains."
-if ! grep --quiet facebook /etc/hosts; then
-    run cat block_facebook | sudo tee -a /etc/hosts
-else
-    echo "${dim}▹ Facebook domains already blocked. $reset"
-fi
 
 # Install Applications
 
@@ -272,36 +116,14 @@ fi
 
 echo "Install and configure git."
 run brew install git
-run git config --global user.email "echohack@users.noreply.github.com"
-git config --global user.name "echohack"
+run git config --global user.email "775209+gungdr@users.noreply.github.com"
+git config --global user.name "gungdr"
 
 echo "Install jq."
 run brew install jq
 
-echo "Install Habitat."
-run brew tap habitat-sh/habitat
-run brew install hab
-run brew upgrade hab
-
-echo "Install mas (Mac App Store Command Line)."
-run brew install mas
-
-echo "Install youtube-dl."
-run brew install youtube-dl
-run brew upgrade youtube-dl
-run brew install ffmpeg
-run brew upgrade ffmpeg
-
-echo "Install keyboard flashing tool for Nightfox Mechanical keyboard."
-run brew install dfu-util
-# Flash with dfu-util -a 0 -R -D kiibohd.dfu.bin
-
-echo "Install exercism CLI."
-run brew install exercism
-run brew upgrade exercism
-
-echo "Install spectacle."
-run brew cask install spectacle
+echo "Install Alfred."
+run brew cask install alfred
 
 echo "Install docker."
 run brew cask install docker
@@ -309,69 +131,8 @@ run brew cask install docker
 echo "Install VLC."
 run brew cask install vlc
 
-echo "Install LiceCap."
-run brew cask install licecap
-
 echo "Install Visual Studio Code."
 run brew cask install visual-studio-code
-
-echo "Install okta_aws tool for Chef Software AWS integration."
-run brew tap chef/okta_aws
-run brew install okta_aws
-
-testing code commit
-echo "Install Visual Studio Code Extensions."
-vscode_install_ext(){
-    run code --install-extension $@
-}
-vscode_install_ext bungcip.better-toml
-vscode_install_ext mauve.terraform
-vscode_install_ext ms-python.python
-vscode_install_ext octref.vetur
-vscode_install_ext rust-lang.rust
-
-# Trust a curl | bash? Why not.
-echo "Install rust using Rustup."
-rustc --version
-if [[ $? != 0 ]] ; then
-    run curl https://sh.rustup.rs -sSf | sh
-    run rustup update
-fi
-
-echo "Install RLS."
-run rustup component add rls-preview rust-analysis rust-src
-
-echo "Install rustfmt."
-rustup component add rustfmt-preview
-
-# Install all the Mac App Store applications using mas. https://github.com/mas-cli/mas
-mac_app_login=$(mas account | grep @)
-if [ -z "$mac_app_login" ] ; then
-    chapter "Let's install Mac App Store applications. What is your Mac App Store email login? $bold"
-    read mac_app_login
-    run mas signin $mac_app_login
-fi
-
-echo "Install Reeder."
-run mas install 880001334
-
-echo "Install Keynote."
-run mas install 409183694
-
-echo "Install Things3."
-run mas install 904280696
-
-echo "Install Slack."
-run mas install 803453959
-
-echo "Install Decompressor."
-run mas install 1033480833
-
-echo "Install Speedtest."
-run mas install 1153157709
-
-echo "Upgrade any Mac App Store applications."
-run mas upgrade
 
 echo "Run one final check to make sure software is up to date."
 run softwareupdate -i -a
